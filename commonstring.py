0f17379c648cd53abb0b1929d8b280e546885d2f
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-05-31 23:38:52 +0530

    Create commonstring.py

---
+++ commonstring.py
@@ -0,0 +1,10 @@
+s1 = input("\nEnter First String : ")
+s2 = input("Enter Second String : ")
+s = ""
+for i in s2 :
+    if i in s1 :
+        pass
+    else :
+        s+=i
+
+print("\nRequired String :",s)
