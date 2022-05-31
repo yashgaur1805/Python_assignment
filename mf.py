3543e9d23d2c92e98aebdbc11dc2a7a21bbd882f
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-05-31 23:55:31 +0530

    Create mf.py

---
+++ mf.py
@@ -0,0 +1,11 @@
+import math
+
+print("\nFor -2.8 :" )
+print("\ntrunc(-2.8) = ",math.trunc(-2.8))
+print("floor(-2.8) = ",math.floor(-2.8))
+print("ceil(-2.8) = ",math.ceil(-2.8))
+
+print("\nFor -0.5 :")
+print("\ntrunc(-0.5) = ",math.trunc(-0.5))
+print("floor(-0.5) = ",math.floor(-0.5))
+print("ceil(-0.5) = ",math.ceil(-0.5))
