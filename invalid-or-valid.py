8e0dbc31e02383481523e5a928ffa1f38a70e261
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-05-31 23:52:02 +0530

    Create invalid-or-valid.py

---
+++ invalid-or-valid.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the 1st angle : "))
+b = int(input("Enter the 2nd angle : "))
+c = int(input("Enter the 3rd angle : "))
+
+if a+b+c == 180 :
+    print("\nTriangle is valid")
+else :
+    print("\nTriangle is invalid")
