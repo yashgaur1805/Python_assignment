ab39b0ffe4107e7c06eb552b473fddbc63f01ce0
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-05-31 23:41:22 +0530

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,10 @@
+print("\nEnter the Coordinates of point 1")
+x1 = float(input())
+y1 = float(input())
+print("\nEnter the coordinates  of point 2")
+x2 = float(input())
+y2 = float(input())
+
+d = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+
+print("\nDistance between Point 1 and Point 2 =",d)
