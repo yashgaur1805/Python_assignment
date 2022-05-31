5ab66810b294849ed82a93e682b794e6c274a364
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-05-31 23:44:16 +0530

    Create eXISTENCE.PY

---
+++ eXISTENCE.PY
@@ -0,0 +1,11 @@
+a , b , c = input("\nEnter three sides : ").split()
+a = int(a); b = int(b) ; c = int(c)
+
+if a>b and a>c and b+c>a :
+    print("\nTriangle exist")
+elif b>a and b>c and a+c>b :
+    print("\nTriangle exist")
+elif c>a and c>b and a+b>c :
+    print("\nTriangle exist")
+else :
+    print("\nTriangle doesn't exist")
