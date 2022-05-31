9e6aaad581ad700b90c04a92efaea8afee6595ae
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-05-31 23:34:02 +0530

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,8 @@
+s = input("\nEnter a string : ").lower()
+s1 = input("Enter another string : ").lower()
+if sorted(s) == sorted(s1) :
+    print("\nIts an anagram")
+else :
+    print("\nIts not an anagram")
+© 2022 GitHub, Inc.
+Terms
